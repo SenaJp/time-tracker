3a3383ef541aaf01f1483abafd73aee8b18e9163
João Pedro G <joaocitimo@gmail.com>
2022-06-30 17:56:37 -0300

    feat: :sparkles: Column project

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,11 @@
 <template>
     <Box>
         <div class="columns">
-            <div class="column is-7">
+            <div class="column is-4">
                 {{task.description || 'Tarefa sem descrição'}}
+            </div>
+            <div class="column is-3">
+                {{ task.project?.name || 'N/D' }}
             </div>
             <div class="column">
                 <StopWatch :timeInSeconds="task.durationInSeconds"/>
